2804e13384a6aeff0b79c02aa5bd7770c7304b39
Akash Patel <akashpatel@Akashs-Mac-mini-3.local>
2025-09-06 20:04:44 +0530

    updated 6sep

--- src/components/Body.js
+++ src/components/Body.js
@@ -8,42 +8,45 @@ import resList from "../utils/mockdata";
 const Body = () => {
    //State Variable -- Super powerful variable
 
+   const[listOfRestaurants,setListOfRestaurants] = useState(resList);
 
 
 
-   let listOfRestaurants = [
-      {
+
+//    let listOfRestaurants = [
+//       {
          
-            info: {
-              id: "1",
-              name: "KFC",
-              cuisines: ["Burgers", "Fast Food"],
-              cloudinaryImageId:
-                "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/5bfe1062-dbda-4cea-962f-ecd60a1d1764_17301.JPG",
-              avgRating: 4.1,
-            },
+//             info: {
+//               id: "1",
+//               name: "KFC",
+//               cuisines: ["Burgers", "Fast Food"],
+//               cloudinaryImageId:
+//                 "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/5bfe1062-dbda-4cea-962f-ecd60a1d1764_17301.JPG",
+//               avgRating: 4.1,
+//             },
           
 
-   },
-   {
+//    },
+//    {
          
-      info: {
-        id: "2",
-        name: "CFK",
-        cuisines: ["Burgers", "Fast Food"],
-        cloudinaryImageId:
-          "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/5bfe1062-dbda-4cea-962f-ecd60a1d1764_17301.JPG",
-        avgRating: 1.1,
-      },
+//       info: {
+//         id: "2",
+//         name: "CFK",
+//         cuisines: ["Burgers", "Fast Food"],
+//         cloudinaryImageId:
+//           "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/5bfe1062-dbda-4cea-962f-ecd60a1d1764_17301.JPG",
+//         avgRating: 1.1,
+//       },
     
 
-}];
+// }];
     return(
      <div className="body">
         <div className="filter">
          <button className="filter-btn" onClick={() => {
-            listOfRestaurants = listOfRestaurants.filter((res) => res.info.avgRating > 4);
+           let upd = listOfRestaurants.filter((res) => res.info.avgRating > 4.2);
             console.log(listOfRestaurants);
+            setListOfRestaurants(upd);
          } 
          
          
